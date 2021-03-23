5f9e6d990df8b9e496a019a5ca4eb671689a124d
Jiang Han <38500283+jianghan1976@users.noreply.github.com>
2021-03-23 17:04:48 +0800

    Update gossipsub.go
    再调大点，看看

--- gossipsub.go
+++ gossipsub.go
@@ -35,16 +35,16 @@ var (
 	// GossipSubD sets the optimal degree for a GossipSub topic mesh. For example, if GossipSubD == 6,
 	// each peer will want to have about six peers in their mesh for each topic they're subscribed to.
 	// GossipSubD should be set somewhere between GossipSubDlo and GossipSubDhi.
-	GossipSubD = 10
+	GossipSubD = 18
 
 	// GossipSubDlo sets the lower bound on the number of peers we keep in a GossipSub topic mesh.
 	// If we have fewer than GossipSubDlo peers, we will attempt to graft some more into the mesh at
 	// the next heartbeat.
-	GossipSubDlo = 8
+	GossipSubDlo = 12
 
 	// GossipSubDhi sets the upper bound on the number of peers we keep in a GossipSub topic mesh.
 	// If we have more than GossipSubDhi peers, we will select some to prune from the mesh at the next heartbeat.
-	GossipSubDhi = 16
+	GossipSubDhi = 24
 
 	// GossipSubDscore affects how peers are selected when pruning a mesh due to over subscription.
 	// At least GossipSubDscore of the retained peers will be high-scoring, while the remainder are
